6c1f34c5e8a38d4013aa343e47ac7922f9a5cbc5
Ssnnaaiill <newdk3025@gmail.com>
2019-11-18 18:22:59 +0900

    :sparkling: new vuex-tutorial project

---
+++ vuex-tutorial/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    Parent counter: {{ count }}
+    <button v-on:click="addCounter">+</button>
+    <button v-on:click="subCounter">-</button>
+    <Child v-bind:passedCounter="counter"></Child>
+  </div>
+</template>
+
+<script>
+import Child from '@/components/Child';
+
+export default {
+  name: 'app',
+  data: () => ({
+    counter: 0,
+  }),
+  components: {
+    Child,
+  },
+  methods: {
+    addCounter() {
+      this.counter++;
+    },
+    subCounter() {
+      this.counter--;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vuex-tutorial/src/components/Child.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <hr />
+    Child Counter: {{ passedCounter }}
+    <br />
+    <button>+</button>
+    <button>-</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    passedCounter: Number,
+  },
+};
+</script>
+
+<style>
+</style>
